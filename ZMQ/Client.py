e5ef3205b1750b56a5d66c57b3f76e5983dc670d
nefariousman <brandonj@kallaher.org>
2014-03-29 18:01:08 -0700

    Updated with more comments

--- ZMQ/Client.py
+++ ZMQ/Client.py
@@ -7,21 +7,26 @@
 import sys
 import zmq
 
-#  Socket to talk to server
+# Set the context of the Client
 context = zmq.Context()
+# Set the socket as a SUBscriber
 socket = context.socket(zmq.SUB)
 
 # Connect to the Server
-# the socket is the connection to the server
+# The socket is the connection to the server
 print("Collecting updates from weather server...")
 socket.connect("tcp://localhost:5556")
 
 # Subscribe to zipcode, default is Pullman, 99163
+# Can be changed by adding arguments to the command line initialization
 zip_filter = sys.argv[1] if len(sys.argv) > 1 else "99163"
 
 # Python 2 - ascii bytes to unicode str
+# Converts the zip_filter from a string to an int
 if isinstance(zip_filter, bytes):
     zip_filter = zip_filter.decode('ascii')
+	
+# Set the options for the socket
 socket.setsockopt_string(zmq.SUBSCRIBE, zip_filter)
 
 # Process 5 updates
@@ -31,6 +36,7 @@ for update_nbr in range(5):
     zipcode, temperature, relhumidity = string.split()
     total_temp += int(temperature)
 
+# Print out the results
 print("Average temperature for zipcode '%s' was %dF" % (
       zip_filter, total_temp / update_nbr)
 )
--- ZMQ/Server.py
+++ ZMQ/Server.py
@@ -7,13 +7,20 @@
 import zmq
 from random import randrange
 
+# Set the context of the server
 context = zmq.Context()
+
+# Set the socket to PUBlish
 socket = context.socket(zmq.PUB)
+
+# Bind the socket to 5556
 socket.bind("tcp://*:5556")
 
 while True:
+	# Randomly generate values
     zipcode = randrange(1, 100000)
     temperature = randrange(-80, 135)
     relhumidity = randrange(10, 60)
 
+	# Send the random values to the SUBscriber/Client as a string
     socket.send_string("%i %i %i" % (zipcode, temperature, relhumidity))
