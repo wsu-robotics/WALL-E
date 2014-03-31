d66725ef2ec5f4f93d270006ebee45bdd8cd19f9
nefariousman <brandonj@kallaher.org>
2014-03-30 19:21:42 -0700

    Added variable speed control

--- xboxdrv-parser/quadcopter_controller_example.py
+++ xboxdrv-parser/quadcopter_controller_example.py
@@ -1,28 +1,34 @@
 from xboxdrv_parser import Controller
 from time import sleep
-#import serial
+import serial
 
 def main ():
     # Get input from the two analog sticks as yaw, throttle, roll, and pitch. Take the (0 - 255) input value and
     # map it to a (-1 - 1) range.
     controller = Controller (["X1", "Y1", "X2", "Y2"], ["yaw", "throttle", "roll", "pitch"], (0, 255), (-1, 1))
-    #ser = serial.Serial ('/./dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial ('/./dev/ttyACM0', 9600, timeout=1)
     while True:
         control_packet = controller.get_values ()
 
         print (control_packet)
-
-        if (control_packet['yaw'] == 1):
-            #ser.write ('d')
+		input = 0
+        if (control_packet['yaw'] <= 1 && control_packet['yaw'] >= 0.05):
+            input *= 255
+			ser.write (input)
             print ("d")
-        elif (control_packet['yaw'] == -1):
-            #ser.write ('a')
+        elif (control_packet['yaw'] >= -1 && control_packet['yaw'] <= -0.05):
+            input *= 255
+			ser.write (input)
             print ("a")
-        elif (control_packet['throttle'] == -1):
-            #ser.write ('w')
+        elif (control_packet['throttle'] >= -1 && control_packet['throttle'] <= -0.08):
+            input *= 255
+			input -= 255
+			ser.write (input)
             print ("w")
-        elif (control_packet['throttle'] == 1):
-            #ser.write ('s')
+        elif (control_packet['throttle'] <= 1 && control_packet['throttle'] >= 0.08):
+            input *= 255
+			input += 255
+			ser.write (input)
             print ("s")
 
         # Update at 20 messages a second
