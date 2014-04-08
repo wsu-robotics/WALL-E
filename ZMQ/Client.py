461cf80092d1cd0f16f7a8b57b5c891c67cd5363
nefariousman <brandonj@kallaher.org>
2014-04-08 10:09:20 -0700

    Integrate Networking and control files

--- ZMQ/Client.py
+++ ZMQ/Client.py
@@ -6,6 +6,9 @@
 
 import sys
 import zmq
+import serial
+
+ser = serial.Serial ('/./dev/tty/ACM0', 9600, timeout=1)
 
 # Set the context of the Client
 context = zmq.Context()
@@ -14,29 +17,32 @@ socket = context.socket(zmq.SUB)
 
 # Connect to the Server
 # The socket is the connection to the server
-print("Collecting updates from weather server...")
+print("Collecting updates from server...")
 socket.connect("tcp://10.99.66.73:5556")
 
-# Subscribe to zipcode, default is Pullman, 99163
-# Can be changed by adding arguments to the command line initialization
-zip_filter = sys.argv[1] if len(sys.argv) > 1 else "99163"
-
-# Python 2 - ascii bytes to unicode str
-# Converts the zip_filter from a string to an int
-if isinstance(zip_filter, bytes):
-    zip_filter = zip_filter.decode('ascii')
-	
 # Set the options for the socket
-socket.setsockopt_unicode(zmq.SUBSCRIBE, zip_filter)
-
-# Process 5 updates
-total_temp = 0
-for update_nbr in range(5):
-    string = socket.recv()
-    zipcode, temperature, relhumidity = string.split()
-    total_temp += int(temperature)
-
-# Print out the results
-print("Average temperature for zipcode '%s' was %dF" % (
-      zip_filter, total_temp / update_nbr)
-)
+socket.setsockopt_unicode(zmq.SUBSCRIBE, "WALLE")
+while True:
+	control_packet = socket.recv_json()
+	#Print out the results
+	print(control_packet)
+	input = 0
+    if (control_packet['yaw'] <= 1 and control_packet['yaw'] >= 0.05):
+        input *= 255
+        input += 255
+        print ("d")
+    elif (control_packet['yaw'] >= -1 and control_packet['yaw'] <= -0.05):
+        input *= 255
+        input -= 255
+        print ("a")
+    elif (control_packet['throttle'] >= -1 and control_packet['throttle'] <= -0.08):
+        input *= 255
+		#make the value intuitive
+        input = math.fabs(input)
+        print ("w")
+    elif (control_packet['throttle'] <= 1 and control_packet['throttle'] >= 0.08):
+        input *= 255
+	    #make the value intuitive
+        input = -input
+        print ("s")
+	ser.write(input)
--- xboxdrv-parser/WALLE-Control.py
+++ xboxdrv-parser/WALLE-Control.py
@@ -1,41 +1,17 @@
 from xboxdrv_parser import Controller
 from time import sleep
-import serial
-
+import zmq
 def main ():
     # Get input from the two analog sticks as yaw, throttle, roll, and pitch. Take the (0 - 255) input value and
     # map it to a (-1 - 1) range.
+	context = zmq.Context()
+	socket = context.socket(zmq.PUB)
+	socket.bind("tcp://*:5556")
     controller = Controller (["X1", "Y1", "X2", "Y2"], ["yaw", "throttle", "roll", "pitch"], (0, 255), (-1, 1))
-    ser = serial.Serial ('/./dev/ttyACM0', 9600, timeout=1)
     while True:
         control_packet = controller.get_values ()
-
+		socket.send_json(control_packet)
         print (control_packet)
-	
-        input = 0
-        if (control_packet['yaw'] <= 1 and control_packet['yaw'] >= 0.05):
-            input *= 255
-            input += 255
-            ser.write (input)
-            print ("d")
-        elif (control_packet['yaw'] >= -1 and control_packet['yaw'] <= -0.05):
-            input *= 255
-            input -= 255
-            ser.write (input)
-            print ("a")
-        elif (control_packet['throttle'] >= -1 and control_packet['throttle'] <= -0.08):
-            input *= 255
-	    #make the value intuitive
-            input = math.fabs(input)
-            ser.write (input)
-            print ("w")
-        elif (control_packet['throttle'] <= 1 and control_packet['throttle'] >= 0.08):
-            input *= 255
-	    #make the value intuitive
-            input = -input
-            ser.write (input)
-            print ("s")
-
         # Update at 20 messages a second
         sleep (.05)
 
